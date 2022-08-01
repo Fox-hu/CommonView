10d7fb2b0a16e84f84539fc02f5194127602b710
hujia <hujia@bilibili.com>
2022-08-01 15:55:02 +0800

    Initial commit

---
+++ app/src/main/java/com/fox/commonview/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.fox.commonview
+
+import com.fox.commonbase.base.BaseListActivity
+
+class MainActivity : BaseListActivity() {
+    override fun initItem() {
+//          activityMap["LockPatternActivity"] = LockPatternActivity::class.java
+    }
+}
